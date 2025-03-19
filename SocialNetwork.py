ed43c3577ec203acc20536bc676a39234518917a
João Augusto da Veiga Carbonera <134736365+JoaoCarbonera@users.noreply.github.com>
2025-03-18 21:01:33 -0300

    Criacao do To String

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -3,3 +3,6 @@ class Socialnetwork:
     def __init__(self, name, description):
         self._name = name
         self._description = description
+    
+    def __str__(self):
+        return f"Socialnetwork(name={self._name}, description={self._description})"
